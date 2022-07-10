afefd37d80b267b3f3b0600b0f2abbaa757d242c
Vladimir Kalinin <Vofanchic@mail.ru>
2022-07-10 11:40:29 +0500

    have problems with vars

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ import sys
 
 
 class EmployeeDlg(QDialog):
-    """Employee dialog."""
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, root, **kwargs): # def __init__(self, parent=None):
+        super().__init__(root, **kwargs) #     super().__init__(parent)
+        self.mywindow = root
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.push_btn)
 
     def fill_dialog(self, lst):
         for co, it in enumerate(lst):
             self.ui.tableWidget.setRowCount(co + 1)
             self.ui.tableWidget.setItem(co, 0, QTableWidgetItem(f"{it[0]}"))
             self.ui.tableWidget.setItem(co, 1, QTableWidgetItem(f"{it[1]}"))
+
+
+    def push_btn(self):
+        self.mywindow.cho = self.ui.tableWidget.currentItem().row()
+
 
 
 class mywindow(QtWidgets.QMainWindow):
@@ -35,9 +41,9 @@ class mywindow(QtWidgets.QMainWindow):
         choice = self.ui.tableWidget.currentItem()
         id = self.rzn_list[choice.row() + 1]['id']
         resp = self.search_class.search_rzn_item(id)
-        dlg = EmployeeDlg(self)
-        dlg.fill_dialog(resp)
-        dlg.exec()
+        self.dlg = EmployeeDlg(self)
+        self.dlg.fill_dialog(resp)
+        self.dlg.exec()
 
     def show_rzn_items(self):
         self.search_rzn = self.ui.lineEdit.text()
@@ -55,6 +61,8 @@ class mywindow(QtWidgets.QMainWindow):
     def button_search_ktru(self):
         # id = self.rzn_list[ret.row() + 1]['id']
         ret = self.ui.tableWidget.currentItem()
+        print(self.cho)
+
         if ret.text().isdigit and len(ret.text()) == 6:
             resp = self.search_class.search_ktru_by_nkmi(ret.text())
             if resp == None:
